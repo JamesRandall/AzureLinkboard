af94f97ae590e69013f3face8912a0a078d635c1
James Randall <james@accidentalfish.com>
2014-07-20 17:27:41 +0100

    Moved add link controller over to new approach

--- Server/AzureLinkboard.Web/app/controllers/indexController.js
+++ Server/AzureLinkboard.Web/app/controllers/indexController.js
@@ -10,22 +10,32 @@
                     "Description": "",
                     "Tags": ""
                 };
+                $scope.saveResult = linkFeedService.defaultModel();
             }
+
+            $scope.saveResult = linkFeedService.defaultModel();
 
             $scope.logOut = function() {
                 authService.logOut();
                 $location.path('/home');
             };
 
-            $scope.showAddLink = function() {
+            $scope.showAddLink = function () {
                 setNewLinkModel();
                 $("#addLinkModal").modal();
             };
 
-            $scope.saveLink = function() {
-                linkFeedService.saveLink($scope.newLink).then(function(results) {
+            $scope.saveLink = function () {
+                if ($scope.linkform.$invalid) {
+                    return;
+                }
+                linkFeedService.saveLink($scope.newLink).then(function (results) {
+                    $scope.saveResult = results;
                     $scope.$broadcast('newLink', results.data);
                     $('#addLinkModal').modal('hide');
+                },
+                function (error) {
+                    $scope.saveResult = error;
                 });
 
             };
--- Server/AzureLinkboard.Web/app/services/linkFeedService.js
+++ Server/AzureLinkboard.Web/app/services/linkFeedService.js
@@ -1,23 +1,30 @@
 ﻿'use strict';
-app.factory('linkFeedService', ['$http', 'settingsService', function ($http, settings) {
+app.factory('linkFeedService', ['$http', '$q', 'settingsService', 'webApiValidationService', function ($http, $q, settings, webApiValidationService) {
 
     var serviceBase = settings.remoteServer;
 
     return {
-        getPage: function (continuationToken) {
+        getPage: function(continuationToken) {
             var queryUrl = serviceBase + 'api/url';
             if (continuationToken !== undefined) {
                 queryUrl = queryUrl + "?continuationToken=" + encodeURIComponent(continuationToken);
-            } 
-            return $http.get(queryUrl).then(function (results) {
+            }
+            return $http.get(queryUrl).then(function(results) {
                 return results;
             });
         },
         saveLink: function(link) {
             var queryUrl = serviceBase + 'api/url';
-            return $http.post(queryUrl, link).then(function(results) {
-                return results;
+            var deferred = $q.defer();
+            $http.post(queryUrl, link).then(function(results) {
+                deferred.resolve(webApiValidationService.handleSuccess(results));
+            }, function(error) {
+                deferred.reject(webApiValidationService.handleError(error));
             });
+            return deferred.promise;
+        },
+        defaultModel: function() {
+            return webApiValidationService.defaultModel();
         }
-    }
+    };
 }]);
